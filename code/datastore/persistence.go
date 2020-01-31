947e770560e615442fe3c041820e48aaa805469d
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-01-31 21:40:59 +0100

    Refactor persistence.go: Split functions, use ioutil

--- code/datastore/persistence.go
+++ code/datastore/persistence.go
@@ -8,26 +8,18 @@ import (
 )
 
 // Save persistently stores the struct s into a file at path as bytes.
-// The implementation closely follows the following tutorial:
+// The implementation follows the following tutorial:
 // https://medium.com/@matryer/golang-advent-calendar-day-eleven-persisting-go-objects-to-disk-7caf1ee3d11d
-// Except that the encoding format used is gob.
-// Use as follows:
+// Unlike in the tutorial, we use ioutil for IO and gob for encoding.
+// Use the function as follows:
 // Save('/path', structure)
 func Save(path string, s interface{}) error {
-	f, err := os.Create(path)
+	serialized, err := Encode(s)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
-	// encode s as bytes
-	var buffer bytes.Buffer
-	enc := gob.NewEncoder(&buffer)
-	err = enc.Encode(s)
-	if err != nil {
-		return err
-	}
-	// write buffer into file
-	f.Write(buffer.Bytes())
+	// os.ModePerm is Unix permissions for everything (0o777)
+	ioutil.WriteFile(path, serialized, os.ModePerm)
 	return nil
 }
 
@@ -41,11 +33,32 @@ func Load(path string, s interface{}) error {
 	if err != nil {
 		return err
 	}
+	// decode structure from bytes
+	err = Decode(contents, s)
+	return nil
+}
+
+// Encode serializes the struct s into an array of bytes.
+func Encode(s interface{}) ([]byte, error) {
 	var buffer bytes.Buffer
-	buffer.Write(contents)
-	// decode buffer into s
-	dec := gob.NewDecoder(&buffer)
-	err = dec.Decode(s)
+	// FIXME: for performance reasons reuse enc across function calls?
+	enc := gob.NewEncoder(&buffer)
+	err := enc.Encode(s)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+// Decode deserializes the buffer of bytes into the struct variable s.
+// s must be a reference
+// For example, use as:
+// Decode(buf, &structure)
+func Decode(buffer []byte, s interface{}) error {
+	b := bytes.NewBuffer(buffer)
+	// FIXME: for performance reasons reuse dec across function calls?
+	dec := gob.NewDecoder(b)
+	err := dec.Decode(s)
 	if err != nil {
 		return err
 	}
