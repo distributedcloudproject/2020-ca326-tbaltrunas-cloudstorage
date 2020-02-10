e7dfd6a5f1a9090c59c3f8adec41b17da7e3f10b
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-10 12:27:39 +0100

    Use TempFile

--- code/datastore/datastore_test.go
+++ code/datastore/datastore_test.go
@@ -7,57 +7,49 @@ import (
 )
 
 func TestFileChunks(t *testing.T) {
-	// create a file with test contents
-	path := "/tmp/cloud_test_file"
-	t.Logf("File path: %s", path)
+	// Create a temporary file with a random string at its end in the default TMP location.
+	tmpfile, err := ioutil.TempFile("", "cloud_test_file_*")
+	if err != nil { t.Error(err) }
+	defer os.Remove(tmpfile.Name())
+	defer tmpfile.Close()
+
+	path := tmpfile.Name()
+	t.Logf("Temporary filepath: %s", path)
 
 	fileContents := "hellothere"  // 10 bytes
-	err := ioutil.WriteFile(path, []byte(fileContents), os.ModePerm)
-	if err != nil {
-		t.Error(err)
-	}
+	_, err = tmpfile.Write([]byte(fileContents))
+	if err != nil { t.Error(err) }
+
 	fileContentsRead, err := ioutil.ReadFile(path)
-	if err != nil {
-		t.Error(err)
-	}
+	if err != nil { t.Error(err) }
 	t.Logf("File contents: %s", string(fileContentsRead))
 
 	NumberOfChunks := 2
 	t.Logf("Operating with chunk number: %d", NumberOfChunks)
 
 	file, err := NewFile(path, NumberOfChunks)
-	if err != nil {
-		t.Error(err)
-	}
+	if err != nil { t.Error(err) }
 	t.Logf("File: %v", file)
 
 	// split the file into two chunks
 	n := 0
 	t.Logf("Chunk: %d", n)
 	contents0, bytesRead, err := file.GetChunk(n)
-	if err != nil {
-		t.Error(err)
-	}
+	if err != nil { t.Error(err) }
 	t.Logf("Bytes read: %d", bytesRead)
 	t.Logf("Contents read: %v (string: %v)", contents0, string(contents0))
 	chunkID, err := file.GetChunkID(n)
-	if err != nil {
-		t.Error(err)
-	}
+	if err != nil { t.Error(err) }
 	t.Logf("ChunkID: %s", chunkID)
 
 	n = 1
 	t.Logf("Chunk: %d", n)
 	contents1, bytesRead, err := file.GetChunk(n)
-	if err != nil {
-		t.Error(err)
-	}
+	if err != nil { t.Error(err) }
 	t.Logf("Bytes read: %d", bytesRead)
 	t.Logf("Contents read: %v (string: %v)", contents1, string(contents1))
 	chunkID, err = file.GetChunkID(n)
-	if err != nil {
-		t.Error(err)
-	}
+	if err != nil { t.Error(err) }
 	t.Logf("ChunkID: %s", chunkID)
 
 	// collect back the chunks and check against original content
