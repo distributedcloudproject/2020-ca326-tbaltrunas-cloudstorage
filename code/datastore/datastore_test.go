bdbf278cdd9248bd8b115126ce79f761f0599105
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-13 12:33:44 +0100

    Add two constructors for File - by numchunks and by chunksize

--- code/datastore/datastore_test.go
+++ code/datastore/datastore_test.go
@@ -12,7 +12,6 @@ func TestFileChunks(t *testing.T) {
 	if err != nil { t.Error(err) }
 	defer os.Remove(tmpfile.Name())
 	defer tmpfile.Close()
-
 	path := tmpfile.Name()
 	t.Logf("Temporary filepath: %s", path)
 
@@ -24,10 +23,10 @@ func TestFileChunks(t *testing.T) {
 	if err != nil { t.Error(err) }
 	t.Logf("File contents: %s", string(fileContentsRead))
 
-	NumberOfChunks := 2
-	t.Logf("Operating with chunk number: %d", NumberOfChunks)
+	NumChunks := 2
+	t.Logf("Operating with chunk number: %d", NumChunks)
 
-	file, err := NewFile(path, NumberOfChunks)
+	file, err := NewFileNumChunks(path, NumChunks)
 	if err != nil { t.Error(err) }
 	t.Logf("File: %v", file)
 
@@ -58,5 +57,39 @@ func TestFileChunks(t *testing.T) {
 	t.Logf("Read contents: %s", retrievedContents)
 	if retrievedContents != fileContents {
 		t.Errorf("Actual and read contents differ.")
+	}
+}
+
+func TestNewFile(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "cloud_test_file_*")
+	if err != nil { t.Error(err) }
+	defer os.Remove(tmpfile.Name())
+	defer tmpfile.Close()
+
+	path := tmpfile.Name()
+	t.Logf("Temporary filepath: %s", path)
+
+	fileContents := "hellothere"  // 10 bytes
+	_, err = tmpfile.Write([]byte(fileContents))
+	if err != nil { t.Error(err) }
+
+	file1, err := NewFileNumChunks(path, 2)  // 2 chunks of 5 bytes each
+	if err != nil { t.Error(err) }
+	t.Logf("File from NumChunks (File 1): %v.", file1)
+
+	file2, err := NewFileChunkSize(path, 5)  // 5 bytes giving 2 chunks
+	if err != nil { t.Error(err) }
+	t.Logf("File from ChunkSize (File 2): %v.", file2)
+
+	t.Logf("File 1 number of chunks: %d.", file1.Chunks.NumChunks)
+	t.Logf("File 2 number of chunks: %d.", file2.Chunks.NumChunks)
+	if file1.Chunks.NumChunks != file2.Chunks.NumChunks {
+		t.Error("NumChunks does not match.")
+	}
+
+	t.Logf("File 1 chunk size: %d.", file1.Chunks.ChunkSize)
+	t.Logf("File 2 chunk size: %d.", file2.Chunks.ChunkSize)
+	if file1.Chunks.ChunkSize != file2.Chunks.ChunkSize {
+		t.Error("ChunkSize does not match.") 
 	}
 }
