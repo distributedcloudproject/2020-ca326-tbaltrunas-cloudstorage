3ec18a84730a04e0e5ab17aa456e319611b57f4b
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-20 16:40:13 +0100

    Comments and rename var

--- code/network/distribution.go
+++ code/network/distribution.go
@@ -9,11 +9,13 @@ import (
 type DistributionScheme map[int][]string
 
 // Distribute computes how to distribute a file and saves the chunks on the required nodes.
+// It calls DistributionAlgorithm to retrieve a DistributionScheme, a mapping telling which nodes should contain which chunks.
+// It then acts on the DistributionScheme to perform the actual requests for saving the chunks.
 func Distribute(file *datastore.File, cloud Cloud) error {
-	redundancyFactor := -1
+	numReplicas := -1
 	antiAffinity := true
 	// TODO: mutex while reading cloud state
-	distributionScheme := DistributionAlgorithm(file, cloud, redundancyFactor, antiAffinity)
+	distributionScheme := DistributionAlgorithm(file, cloud, numReplicas, antiAffinity)
 	// TODO: Actual distribution algorithm. For now we copy all chunks to each node.
 
 	for sequenceNumber, nodeIDs := range distributionScheme {
@@ -31,14 +33,19 @@ func Distribute(file *datastore.File, cloud Cloud) error {
 	return nil
 }
 
-func DistributionAlgorithm(file *datastore.File, cloud Cloud, redundancyFactor int, antiAffinity bool) DistributionScheme {
-	if redundancyFactor == -1 {
+// DistributionAlgorithm returns a suitable DistributionScheme for the file and the given cloud.
+// It acts with two goals in mind: reliability (redundancy) and efficiency.
+// numReplicas specifies how many copies of all file's chunks should be stored on the cloud.
+// antiAffinity specifies whether to avoid storing replicas of the same chunk on the same node.
+// The function also uses node benchmarking to achieve best efficiency (load balancing storage, and using the fastest network).
+func DistributionAlgorithm(file *datastore.File, cloud Cloud, numReplicas int, antiAffinity bool) DistributionScheme {
+	if numReplicas == -1 {
 		return DistributionAll(file, cloud)
-	} else {
-		return nil
 	}
+	return nil
 }
 
+// DistributionAll specifies to store a copy of the file on each node.
 func DistributionAll(file *datastore.File, cloud Cloud) DistributionScheme {
 	scheme := make(map[int][]string)
 	allNodeIDs := make([]string, 0)
