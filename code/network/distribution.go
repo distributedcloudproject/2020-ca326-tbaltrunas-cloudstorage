1f0c60e9ed07973fbae86dc8baa9924ef9edd1f7
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-20 16:28:21 +0100

    Data structure for how to distribute chunks

--- code/network/distribution.go
+++ code/network/distribution.go
@@ -10,14 +10,18 @@ type DistributionScheme map[int][]string
 
 // Distribute computes how to distribute a file and saves the chunks on the required nodes.
 func Distribute(file *datastore.File, cloud Cloud) error {
+	redundancyFactor := -1
+	antiAffinity := true
+	// TODO: mutex while reading cloud state
+	distributionScheme := DistributionAlgorithm(file, cloud, redundancyFactor, antiAffinity)
 	// TODO: Actual distribution algorithm. For now we copy all chunks to each node.
-	nodes := cloud.Network().Nodes
-	for _, n := range nodes {
-		for i, _ := range file.Chunks.Chunks {
-			utils.GetLogger().Printf("[INFO] Saving chunk: %v on node %v.", i, n.Name)
-			cnode := cloud.GetCloudNode(n.ID)
+
+	for sequenceNumber, nodeIDs := range distributionScheme {
+		for _, nodeID := range nodeIDs {
+			cnode := cloud.GetCloudNode(nodeID)
 			if cnode != nil {
-				err := cnode.SaveChunk(file, i)
+				utils.GetLogger().Printf("[INFO] Saving chunk: %v on node %v.", sequenceNumber, nodeID)
+				err := cnode.SaveChunk(file, sequenceNumber)
 				if err != nil {
 					return err
 				}
@@ -28,5 +32,21 @@ func Distribute(file *datastore.File, cloud Cloud) error {
 }
 
 func DistributionAlgorithm(file *datastore.File, cloud Cloud, redundancyFactor int, antiAffinity bool) DistributionScheme {
-	return nil
+	if redundancyFactor == -1 {
+		return DistributionAll(file, cloud)
+	} else {
+		return nil
+	}
+}
+
+func DistributionAll(file *datastore.File, cloud Cloud) DistributionScheme {
+	scheme := make(map[int][]string)
+	allNodeIDs := make([]string, 0)
+	for _, n := range cloud.Network().Nodes {
+		allNodeIDs = append(allNodeIDs, n.ID)
+	}
+	for i, _ := range file.Chunks.Chunks {
+		scheme[i] = allNodeIDs
+	}
+	return scheme
 }
