b3936760ba50ddbd546e94ea8576dcfcb0351ad1
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-21 15:10:05 +0100

    Another comment forgot

--- code/utils/files.go
+++ code/utils/files.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 )
 
+// https://stackoverflow.com/questions/32482673/how-to-get-directory-total-size
 func DirSize(path string) (int64, error) {
 	var size int64
 	err := filepath.Walk(path, func(subpath string, info os.FileInfo, err error) error {
