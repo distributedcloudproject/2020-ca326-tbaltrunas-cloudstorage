e65fd972041c6a0b4b1097bc7ebf6feae463dfd4
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-21 17:40:00 +0100

    Set up React JS project

---
+++ code/web-client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Button>
+        Connect
+      </Button>
+    </div>
+  );
+}
+
+export default App;
