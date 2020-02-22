e48bb5e9b14e07d6c5ccb44af059883ccf215c47
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-22 22:17:27 +0100

    Add 1 more test case (storage based loading)

--- code/network/distribution_test.go
+++ code/network/distribution_test.go
@@ -7,37 +7,63 @@ import (
 	"testing"
 )
 
+// map from chunk idx to node idx's
+type testCaseDistribution map[int][]int
+
 func TestDistribution(t *testing.T) {
 	numNodes := 2
-	clouds, err := CreateTestClouds(numNodes)
-	if err != nil {
-		t.Fatal(err)
-	}
-	cloud := clouds[0]
-
 	chunkSize := 5
+
 	testCases := []struct {
 		Contents 			string
 		StorageCapacities 	[]int64
 		NumReplicas 		int
 		AntiAffinity 		bool
 
-		Distribution 		map[int][]string // map from chunk idx to node ID's
+		Distribution 		testCaseDistribution
 	}{
 		{
 			Contents: "hellothere", // 2 chunks
 			StorageCapacities: []int64{100, 100},
 			NumReplicas: 0,
 			AntiAffinity: true,
-			Distribution: map[int][]string{
-				0: []string{cloud.Network().Nodes[0].ID},
-				1: []string{cloud.Network().Nodes[1].ID},				
+			Distribution: testCaseDistribution{
+				0: []int{0},
+				1: []int{1},				
 			},
 		},
+		{
+			Contents: "hellothere", // 2 chunks
+			StorageCapacities: []int64{500, 100},
+			NumReplicas: 0,
+			AntiAffinity: true,
+			Distribution: testCaseDistribution{
+				0: []int{0},
+				1: []int{0},				
+			},
+		},
+		// {
+		// 	Contents: "hello", // 1 chunk
+		// 	StorageCapacities: []int64{500, 100},
+		// 	NumReplicas: 1, // 2 chunks
+		// 	AntiAffinity: true,
+		// 	Distribution: testCaseDistribution{
+		// 		0: []int{0, 1},				
+		// 	},
+		// },
 	}
 
 	// Distribute file
 	for i, testCase := range testCases {
+		clouds, err := CreateTestClouds(numNodes)
+		if err != nil {
+			t.Fatal(err)
+		}
+		cloud := clouds[0]
+		for i, n := range 	cloud.Network().Nodes {
+			t.Logf("Node %d: %v.", i + 1, n.ID)
+		}
+
 		tmpfile, err := utils.GetTestFile("cloud_test_file_*", []byte(testCase.Contents))
 		if err != nil {
 			t.Fatal(err)
@@ -73,11 +99,23 @@ func TestDistribution(t *testing.T) {
 		for idx, _ := range testCase.Distribution {
 			expectedChunkIDs = append(expectedChunkIDs, file.Chunks.Chunks[idx].ID)
 		}
+
+		expectedNodeIDs := make([][]string, 0)
+		for _, nodeIDsx := range testCase.Distribution {
+			nodeIDs := make([]string, 0)
+			for _, idx := range nodeIDsx {
+				nodeIDs = append(nodeIDs, cloud.Network().Nodes[idx].ID)
+			}
+			expectedNodeIDs = append(expectedNodeIDs, nodeIDs)
+		}
+
+		t.Logf("case(%d) ChunkNodes %v.", i, cloud.Network().ChunkNodes)
 		if len(testCase.Distribution) != len(cloud.Network().ChunkNodes) {
 			t.Errorf("case(%d).Distribution got length %d; want %d", i, len(testCase.Distribution), len(cloud.Network().ChunkNodes))
 		}
-		for chunkIDx, eNodeIDs := range testCase.Distribution {
+		for chunkIDx, _ := range testCase.Distribution {
 			eChunkID := expectedChunkIDs[chunkIDx]
+			eNodeIDs := expectedNodeIDs[chunkIDx]
 			nodeIDs, ok := cloud.Network().ChunkNodes[eChunkID]
 			if !ok {
 				t.Errorf("case(%d).Distribution missing chunk %v", i, eChunkID)
