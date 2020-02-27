928947d89459226a3bada037803776b79495ff5c
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-27 12:25:56 +0100

    WIP: File explorer UI

--- code/web-client/src/components/Home.js
+++ code/web-client/src/components/Home.js
@@ -3,30 +3,21 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Logout from './Logout';
+import FileExplorer from './FileExplorer';
 
 export default function Home(props) {
     return (
         <Container className='d-flex flex-column border p-1'>
-            <Row className='border m-2 p-2'>
-                <Col className='border' large={10}>
-                    <p>Network Name</p>
-                </Col>
-                <Col className='border' large={10}>
-                    <Logout />
-                </Col>
+            <Row className='border m-2 p-2 d-flex justify-content-around'>
+                <p>Network Name</p>
+                <Logout />
             </Row>
             <Container className='d-flex flex-wrap border'>
                 <Col className='border m-1 p-1'>
                     <p>Controls</p>
                 </Col>
                 <Col className='border m-1 p-1' xs={10}>
-                    <p>File browser</p>
-                    <p>File</p>
-                    <p>File</p>
-                    <p>File</p>
-                    <p>File</p>
-                    <p>File</p>
-                    <p>File</p>
+                    <FileExplorer />
                 </Col>
             </Container>
         </Container>
