6b6c997d657e9149fffea5197c88827cfb23d237
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-02-28 21:47:47 +0100

    Move UI handlers to a separate file

--- code/web-client/src/components/FileExplorer/FileExplorer.js
+++ code/web-client/src/components/FileExplorer/FileExplorer.js
@@ -7,9 +7,7 @@ import * as FileExplorerIcons from './Icons';
 import './FileExplorer.css';
 import Upload from './Upload';
 import { FilesAPI } from '../../api';
-
-// import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-
+import * as UpdateUI from './UpdateUI';
 
 export default class FileExplorer extends React.Component {
     state = {
@@ -21,125 +19,45 @@ export default class FileExplorer extends React.Component {
         this.setState({ files: files })
       })
     }
-  
-    // The handlers have been attempted from https://github.com/uptick/react-keyed-file-browser.
 
     // handleCreateFiles adds new files to existing files.
     // files is an Array of DOM File objects (created by input[type=file]).
     handleCreateFiles = (files) => {
-      const prefix = ''; // TODO: handle prefix
       FilesAPI.CreateFile()
-      this.setState(state => {
-        const newFiles = files.map((file) => {
-          let newKey = prefix
-          if (prefix !== '' && prefix.substring(prefix.length - 1, prefix.length) !== '/') {
-            newKey += '/'
-          }
-          newKey += file.name
-          return {
-            key: newKey,
-            size: file.size,
-            // modified: +Moment(),
-          }
-        })
-        const uniqueNewFiles = []
-        newFiles.map((newFile) => {
-          let exists = false
-          state.files.map((existingFile) => {
-            if (existingFile.key === newFile.key) {
-              exists = true
-            }
-          })
-          if (!exists) {
-            uniqueNewFiles.push(newFile)
-          }
-        })
-        state.files = state.files.concat(uniqueNewFiles)
-        return state
-      })
+      const prefix = ''; // TODO: handle prefix
+      this.setState({state: UpdateUI.UpdateUICreateFiles(this.state, files, prefix)})
     }
 
     // handleRenameFile renames an existing file.
     handleRenameFile = (oldKey, newKey) => {
       FilesAPI.UpdateFile()
-      this.setState(state => {
-        const newFiles = []
-        state.files.map((file) => {
-          if (file.key === oldKey) {
-            newFiles.push({
-              ...file,
-              key: newKey,
-            //   modified: +Moment(),
-            })
-          } else {
-            newFiles.push(file)
-          }
-        })
-        state.files = newFiles
-        return state
-      })
+      this.setState({state: UpdateUI.UpdateUIRenameFile(this.state, oldKey, newKey)})
     }
 
     // handleDeleteFile deletes an existing file.
     handleDeleteFile = (fileKey) => {
       FilesAPI.DeleteFile()
-      this.setState(state => {
-        const newFiles = []
-        state.files.map((file) => {
-          if (file.key !== fileKey) {
-            newFiles.push(file)
-          }
-        })
-        state.files = newFiles
-        return state
-      })
+      // FIXME: doesn't get deleted.
+      this.setState({state : UpdateUI.UpdateUIDeleteFile(this.state, fileKey)})
     }
 
     // handleCreateFolder creates a new folder.
     handleCreateFolder = (key) => {
       FilesAPI.CreateFolder()
-      this.setState(state => {
-        state.files = state.files.concat([{
-          key: key,
-        }])
-        return state
-      })
+      this.setState({state: UpdateUI.UpdateUICreateFolder(this.state, key)})
     }
 
     // handleRenameFolder renames an existing folder.
     handleRenameFolder = (oldKey, newKey) => {
       FilesAPI.UpdateFolder()
-      this.setState(state => {
-        const newFiles = []
-        state.files.map((file) => {
-          if (file.key.substr(0, oldKey.length) === oldKey) {
-            newFiles.push({
-              ...file,
-              key: file.key.replace(oldKey, newKey),
-            //   modified: +Moment(),
-            })
-          } else {
-            newFiles.push(file)
-          }
-        })
-        state.files = newFiles
-        return state
-      })
+      this.setState({state: UpdateUI.UpdateUIRenameFolder(this.state, oldKey, newKey)})
     }
 
     // handleDeleteFolder deletes an existing folder.
     handleDeleteFolder = (folderKey) => {
       FilesAPI.DeleteFolder()
-      this.setState(state => {
-        const newFiles = []
-        state.files.map((file) => {
-          if (file.key.substr(0, folderKey.length) !== folderKey) {
-            newFiles.push(file)
-          }
-        })
-        state.files = newFiles
-        return state
-      })
+      // FIXME: doesn't get deleted.
+      this.setState({state: UpdateUI.UpdateUIDeleteFolder(this.state, folderKey)})
     }
   
     render() {
---
+++ code/web-client/src/components/FileExplorer/Upload.js
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import * as FileExplorerIcons from './Icons';
+
+export default function Upload(props) {
+    // Attach refs to component.
+    const fileUploaderEl = useRef(null);
+
+    function handleButtonClick(e) {
+        fileUploaderEl.current.click();
+    }
+
+    function handleInputChange(e) {
+        e.preventDefault();
+        console.log('Received files: ' + e.target.files)
+        const filesArray = Array.from(e.target.files)
+        props.callback(filesArray)
+    }
+
+    // TODO: Upload multiple files or a folder.
+    // TODO: Upload field or box.
+    return (
+        <Button 
+            className="icon-button"
+            onClick={handleButtonClick} >
+            <Form.Control 
+                type="file" 
+                id="file" 
+                ref={fileUploaderEl} 
+                hidden
+                onChange={handleInputChange} >
+            </Form.Control>
+            <FileExplorerIcons.Upload />
+            Upload
+        </Button>
+    );
+}
