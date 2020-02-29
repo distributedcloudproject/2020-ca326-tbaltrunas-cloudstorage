d05a7dd881cce381876782a775090775e42a4b13
Bartosz Swituszak <bartosz.swituszak2@mail.dcu.ie>
2020-02-29 15:40:59 +0000

    Add ChunkStore, change file path to file name

--- code/datastore/datastore.go
+++ code/datastore/datastore.go
@@ -20,7 +20,7 @@ type FileIOReader io.ReaderAt
 
 // File represents a user's file stored on the cloud.
 type File struct {
-	ID FileID // ID of the file (hash of file contents TODO: and a random string for uniqueness).
+	ID FileID // ID of the file (hash of chunk IDs).
 
 	Name string // The name of the file.
 
@@ -44,9 +44,21 @@ type Chunks struct {
 type Chunk struct {
 	ID ChunkID // Unique ID of the chunk (hash value of the contents).
 
-	SequenceNumber int // Chunk sequence used to place the chunk in the correct position in the file.
+	SequenceNumber int   // Chunk sequence used to place the chunk in the correct position in the file.
+	ContentSize    int   // Number of bytes of actual content.
+	ChunkOffset    int64 // The offset of the chunk of the file. (e.g, if offset is 1024, the chunk is contained at
+	// position 1024 of the file.
+}
 
-	ContentSize int // Number of bytes of actual content.
+// ChunkStore represents a locally stored Chunk.
+type ChunkStore struct {
+	Chunk  Chunk
+	FileID FileID
+
+	// StoredAsFile states if the chunk is stored as part of the original file or individually. If it's stored as part
+	// of the file, then the location of the chunk in that file needs to be located before reading.
+	StoredAsFile bool
+	FilePath     string
 }
 
 // DataStore represents a collection of files.
@@ -58,22 +70,23 @@ type DataStore struct {
 // reader is an IO reader that provides access to the underlying file contents.
 // path is the expected filepath of the file, used for directory tree purposes.
 // chunkSize is the number of bytes that each chunk should be at maximum.
-func NewFile(reader FileIOReader, path string, chunkSize int) (*File, error) {
+func NewFile(reader FileIOReader, name string, chunkSize int) (*File, error) {
 	// validate arguments
 	if chunkSize <= 0 {
 		return nil, errors.New("Chunk size must be a positive integer.")
 	}
 
 	// generate each chunk
-	file := new(File)
-	chunks := make([]Chunk, 0)
+	var chunks = Chunks{
+		ChunkSize: chunkSize,
+	}
+
 	i := 0
-	var offset int64
 	allContents := make([]byte, 0)
 	buffer := make([]byte, chunkSize)
 	stop := false
 	for !stop {
-		offset = int64(chunkSize * i)
+		offset := int64(chunkSize * i)
 		numRead, err := reader.ReadAt(buffer, offset)
 		if err == io.EOF && numRead == 0 {
 			// EOF and read nothing
@@ -89,31 +102,31 @@ func NewFile(reader FileIOReader, path string, chunkSize int) (*File, error) {
 		chunk := Chunk{
 			ID:             chunkID,
 			SequenceNumber: i,
+			ChunkOffset:    offset,
 			ContentSize:    numRead,
 		}
-		allContents = append(allContents, buffer...)
-		chunks = append(chunks, chunk)
+		allContents = append(allContents, buffer[:chunk.ContentSize]...)
+		chunks.Chunks = append(chunks.Chunks, chunk)
+		chunks.NumChunks++
 		i++
 	}
 
-	// compute file hash
-	id := utils.HashFile(allContents)
+	// Compute file ID by hashing all of the chunk IDs.
+	id := generateFileID(chunks.Chunks)
 
 	// compute extra information
-	fileSize := file.Chunks.ComputeFileSize()
-	numChunks := len(chunks)
+	fileSize := chunks.ComputeFileSize()
 	// Alternatively:
 	// numChunks*chunkSize ~= fileSize
 	// numChunks ~= ceil(fileSize/chunkSize)
 
-	file.reader = reader
-	file.ID = FileID(id)
-	file.Name = path
-	file.Size = FileSize(fileSize)
-	file.Chunks.NumChunks = numChunks
-	file.Chunks.ChunkSize = chunkSize
-	file.Chunks.Chunks = chunks
-	return file, nil
+	return &File{
+		ID:     FileID(id),
+		reader: reader,
+		Name:   name,
+		Size:   FileSize(fileSize),
+		Chunks: chunks,
+	}, nil
 }
 
 // Contains returns whether the datastore contains the specified file.
@@ -138,4 +151,12 @@ func (ds *DataStore) ContainsName(name string) bool {
 // Add appends a file to the datastore.
 func (ds *DataStore) Add(file *File) {
 	ds.Files = append(ds.Files, file)
+}
+
+func generateFileID(chunks []Chunk) string {
+	buffer := make([]byte, 0)
+	for _, c := range chunks {
+		buffer = append(buffer, c.ID...)
+	}
+	return utils.HashFile(buffer)
 }
