6f098aac466dcfc68274be7c94da47ed0cbd1004
Tomas Baltrunas <tomas.baltrunas2@mail.dcu.ie>
2020-03-01 01:49:07 +0100

    Add token refresh endpoint

--- code/network/httpbackend.go
+++ code/network/httpbackend.go
@@ -8,13 +8,19 @@ import (
 	"fmt"
 	"encoding/json"
 	"time"
+	"os"
 
 	"github.com/gorilla/mux"
 	gorillaHandlers "github.com/gorilla/handlers"
 	"github.com/dgrijalva/jwt-go"
 )
 
-var jwtSecret = []byte("bigsecret")
+const (
+	accessTokenKey = "access_token"
+	accessTokenExpirationTime = 5 * time.Minute
+)
+
+var jwtKey = []byte("bigsecret")
 
 type File struct {
 	Key		string   `json:"key"`
@@ -32,6 +38,7 @@ func (c *cloud) ListenAndServeHTTP(port int) error {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/auth", c.WebAuthenticationHandler)
+	r.HandleFunc("/auth/refresh", c.AuthRefreshHandler)
 
 	r.HandleFunc("/netinfo", c.NetworkInfoHandler)
 	r.HandleFunc("/files", c.GetFiles).Methods(http.MethodGet)
@@ -40,7 +47,6 @@ func (c *cloud) ListenAndServeHTTP(port int) error {
 		Queries("filter", "contents")
 	r.HandleFunc("/files", c.CreateFile).Methods(http.MethodPost)
 
-
 	http.Handle("/", r)
 
 	// Set up CORS middleware for local development.
@@ -48,7 +54,10 @@ func (c *cloud) ListenAndServeHTTP(port int) error {
 	methodsOk := gorillaHandlers.AllowedMethods([]string{http.MethodOptions, http.MethodGet, http.MethodPost})
 
 	utils.GetLogger().Printf("[INFO] HTTP backend listening on address: %s.", address)
-	return http.ListenAndServe(address, gorillaHandlers.CORS(originsOk, methodsOk)(r))
+	return http.ListenAndServe(address, 
+			gorillaHandlers.LoggingHandler(os.Stdout, 
+				gorillaHandlers.CORS(originsOk, methodsOk)(r)))
+	// TODO: use utils.GetLogger() writer
 }
 
 func (c *cloud) WebAuthenticationHandler(w http.ResponseWriter, req *http.Request) {
@@ -57,7 +66,7 @@ func (c *cloud) WebAuthenticationHandler(w http.ResponseWriter, req *http.Reques
 	// TODO: verify sent request, i.e. username and password
 
 	// expires in 5 minutes
-	expirationTime := time.Now().Add(1 * time.Minute)
+	expirationTime := time.Now().Add(accessTokenExpirationTime)
 	authClaims := AuthClaims{
 		Username: "admin", // TODO: roles based on username
 		StandardClaims: jwt.StandardClaims {
@@ -66,7 +75,7 @@ func (c *cloud) WebAuthenticationHandler(w http.ResponseWriter, req *http.Reques
 	}
 	// TODO: decide on the signing method, i.e. use ssh key
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, authClaims)
-	tokenString, err := token.SignedString(jwtSecret)
+	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
 		utils.GetLogger().Printf("[ERROR] %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -74,10 +83,77 @@ func (c *cloud) WebAuthenticationHandler(w http.ResponseWriter, req *http.Reques
 	}
 
 	http.SetCookie(w, &http.Cookie{
-		Name: "access_token",
+		Name: accessTokenKey,
 		Value: tokenString,
 		Expires: expirationTime,
 	})
+}
+
+// Adapted from: https://www.sohamkamani.com/blog/golang/2019-01-01-jwt-authentication/
+func (c *cloud) AuthRefreshHandler(w http.ResponseWriter, req *http.Request) {
+	// (BEGIN) The code uptil this point is the same as the first part of the `Welcome` route
+	cookie, err := req.Cookie(accessTokenKey)
+	if err != nil {
+		utils.GetLogger().Printf("[ERROR] %v", err)
+		if err == http.ErrNoCookie {
+			// Caller did not authenticate in the first place.
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// Other error.
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenString := cookie.Value
+
+	claims := &AuthClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, jwtKeyFunc)
+	if err != nil {
+		utils.GetLogger().Printf("[ERROR] %v", err)
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !token.Valid {
+		utils.GetLogger().Printf("[ERROR] %v", err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// (END) The code up-till this point is the same as the first part of the `Welcome` route
+
+	// New token is only issued if the current token will expire in less than 30 seconds.
+	throttle := 30 * time.Second
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > throttle {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Update the expiration time.
+	expirationTime := time.Now().Add(accessTokenExpirationTime)
+	claims.ExpiresAt = expirationTime.Unix()
+
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	newTokenString, err := newToken.SignedString(jwtKey)
+	if err != nil {
+		utils.GetLogger().Printf("[ERROR] %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set the new token as the users `token` cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:    accessTokenKey,
+		Value:   newTokenString,
+		Expires: expirationTime,
+	})
+}
+
+func jwtKeyFunc(token *jwt.Token) (interface{}, error) {
+	return jwtKey, nil
 }
 
 func (c *cloud) NetworkInfoHandler(w http.ResponseWriter, req *http.Request) {
